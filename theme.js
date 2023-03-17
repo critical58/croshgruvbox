96861ab9356a5eede251926e86e05c1af62ff4ee
critical58 <64205159+critical58@users.noreply.github.com>
2023-03-17 14:38:55 +0000

    gruvboxxx

---
+++ theme.js
@@ -0,0 +1,7 @@
+term_.prefs_.set('foreground-color', "#EBDBB2");
+term_.prefs_.set('background-color', "#282828");
+term_.prefs_.set('color-palette-overrides',
+[ "#928374", "#CC231D", "#98971A", "#D79A21", 
+"#458588", "#B16286", "#689D6A", "#DEDBD7",
+"#D79921", "#6AAAAD", "#FF5555", "#FF6E67",
+"#50FA7B", "#8BE9FD", "#5AF78E", "#000000" ]);
